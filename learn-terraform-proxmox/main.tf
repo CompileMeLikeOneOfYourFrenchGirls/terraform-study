5694b5290e64e7aafcb3cfdc9521e42f9ce51532
CompileMeLikeOneOfYourFrenchGirls <spk91jpn@gmail.com>
2023-09-08 17:29:53 +0200

    Aggiunto provider telmate/proxmox

--- learn-terraform-proxmox/main.tf
+++ learn-terraform-proxmox/main.tf
@@ -1,3 +1,13 @@
+terraform {
+	required_providers {
+		proxmox = {
+			source = "telmate/proxmox"
+			version = "2.9.14"
+		}
+	}
+}
+
 provider "proxmox" {
 	pm_api_url = "https://172.17.0.1:8006/api2/json"
+	pm_tls_insecure = true
 }
